406a0910b7ac18033c4f214513a8ae99c8d0c17a
Christella <christella.cidolit@polymtl.ca>
2018-10-29 22:51:04 -0400

    Adding full grid

--- GraphicTest/Tetris/Main.py
+++ GraphicTest/Tetris/Main.py
@@ -1,20 +1,21 @@
 import arcade
 
-SCREEN_W = 600
+SCREEN_W = 800
 SCREEN_H = 600
 
 arcade.open_window(SCREEN_W, SCREEN_H, "First shot !")
 arcade.set_background_color((51, 153, 255))
 arcade.start_render()
 
-# Board
-arcade.draw_rectangle_outline(300, 300, 402, 552, arcade.color.CERULEAN_BLUE, 2)
+# Board 10*12
+arcade.draw_rectangle_outline(300, 300, 402, 482, arcade.color.CERULEAN_BLUE, 2)
 # Square
 # arcade.draw_rectangle_filled(120, 45, 40, 40, arcade.color.BABY_BLUE)
 
-for i in range(10):
-    arcade.draw_rectangle_filled(120+(i*40), 45, 40, 40, arcade.color.BABY_BLUE)
-    arcade.draw_rectangle_outline(120+(i*40), 45, 40, 40, arcade.color.CORNFLOWER_BLUE, 1)
+for row in range(12):
+    for column in range(10):
+        arcade.draw_rectangle_filled(120+(column*40), 80+(row*40), 40, 40, arcade.color.BABY_BLUE)
+        arcade.draw_rectangle_outline(120+(column*40), 80+(row*40), 40, 40, arcade.color.CORNFLOWER_BLUE, 1)
 
 arcade.finish_render()
 arcade.run()
